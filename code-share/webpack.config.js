3f3ebb8e4a270935d8d790d9b2954403dd0c22d1
aniljos <anil.jos@gmail.com>
2024-06-10 06:28:14 +0530

    created

--- code-share/webpack.config.js
+++ code-share/webpack.config.js
@@ -1,37 +1,38 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  mode: 'development',
-  devServer: {
-    port: 3000,
-    open: true, // Add this line to open the browser automatically
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'], // Add .jsx extension
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?jsx$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', 
-              ['@babel/preset-react', { runtime: 'automatic' }]
-            ],
-            plugins: ['@babel/plugin-transform-runtime'],
-          },
-        },
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
+    mode: 'development',
+    devServer: {
+        port: 3200,
+        open: true, // Add this line to open the browser automatically
+        historyApiFallback: { index: "/", disableDotRule: true },
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'], // Add .jsx extension
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$|jsx/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', ['@babel/preset-react', { runtime: 'automatic' }]],
+                        plugins: ['@babel/plugin-transform-runtime'],
+                    },
+                },
+            },
+            {
+                test: /\.(sass|css|scss)$/,
+                use: ['style-loader', 'css-loader',{
+                  loader: "postcss-loader",
+                }],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    })
-  ],
 };
